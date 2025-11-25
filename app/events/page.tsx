d5e813b7b2e47b15cc3e4efb5f3d29d7fd074b48
Jackie <newyorkracingclub@gmail.com>
2025-11-24 19:03:59 -0500

    Retrieved all events from calendar

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,43 +1,34 @@
-import Head from 'next/head';
-import AllEvents from '@/components/AllEvents';
+import { Metadata } from 'next';
+import AllEvents from '@/components/AllEventsClient';
+import { fetchEvents } from '@/lib/events';
 
-export default function AllEventsPage() {
+export const metadata: Metadata = {
+  title: 'Events - New York Racing Club | Past & Upcoming Races',
+  description:
+    'Explore both past and upcoming events organized by the New York Racing Club. Find details on fixed gear crits, cycling races, running events, and more. Stay updated with our latest activities and past highlights.',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Events - New York Racing Club | Past & Upcoming Races',
+    description:
+      'Discover past and upcoming events by the New York Racing Club. From fixed gear crits and cycling races to running and swimming competitions, explore all the details and stay informed about our activities.',
+    images: ['/logos/landingpage.webp'],
+    url: 'https://www.nyracing.org/events',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Events - New York Racing Club | Past & Upcoming Races',
+    description:
+      'Find out about past and upcoming events organized by the New York Racing Club. View details on various races and competitions, including fixed gear crits and more.',
+    images: ['/logos/landingpage.webp'],
+  },
+};
+
+export default async function AllEventsPage() {
+  const allEvents = await fetchEvents();
+
   return (
-    <>
-      <Head>
-        <title>Events - New York Racing Club | Past & Upcoming Races</title>
-        <meta
-          name="description"
-          content="Explore both past and upcoming events organized by the New York Racing Club. Find details on fixed gear crits, cycling races, running events, and more. Stay updated with our latest activities and past highlights."
-        />
-        <meta name="robots" content="index, follow" />
-        <meta name="googlebot" content="index, follow" />
-
-        <meta
-          property="og:title"
-          content="Events - New York Racing Club | Past & Upcoming Races"
-        />
-        <meta
-          property="og:description"
-          content="Discover past and upcoming events by the New York Racing Club. From fixed gear crits and cycling races to running and swimming competitions, explore all the details and stay informed about our activities."
-        />
-        <meta property="og:image" content="/logos/landingpage.webp" />
-        <meta property="og:url" content="https://www.nyracing.org/events" />
-
-        <meta
-          name="twitter:title"
-          content="Events - New York Racing Club | Past & Upcoming Races"
-        />
-        <meta
-          name="twitter:description"
-          content="Find out about past and upcoming events organized by the New York Racing Club. View details on various races and competitions, including fixed gear crits and more."
-        />
-        <meta name="twitter:image" content="/logos/landingpage.webp" />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <div className="px-6 py-4 md:p-10">
-        <AllEvents />
-      </div>
-    </>
+    <div className="px-6 py-4 md:p-10">
+      <AllEvents events={allEvents} />
+    </div>
   );
 }

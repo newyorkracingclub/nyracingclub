b06f8d1e60db91056587ef48ffe0cf92be3d4660
Jackie <newyorkracingclub@gmail.com>
2026-01-16 09:18:14 -0500

    Added dynamic footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,6 +3,8 @@ import Instagram from './Instagram';
 import Strava from './Strava';
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
+  
   return (
     <footer className="static bottom-0 py-8 right-0 left-0 bg-blue">
       <div className="flex items-center justify-center gap-x-6">
@@ -11,7 +13,7 @@ function Footer() {
         <Strava />
       </div>
       <p className="flex text-xs text-start justify-center pt-4">
-        ©2024 New York Racing Club. All Rights Reserved. <br /> 501(c)(3)
+        ©{currentYear} New York Racing Club. All Rights Reserved. <br /> 501(c)(3)
         Non-Profit Organization.
       </p>
     </footer>
